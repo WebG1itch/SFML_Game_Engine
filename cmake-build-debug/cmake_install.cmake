4bf1c2f97f85b6542886ec3fc6cd7a36fbd7bcba
Jake <jakegibson0228@gmail.com>
2023-05-17 16:34:42 -0400

    Updated for proper project naming

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/CrystalienPlus")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/SFML_Game_Engine")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
